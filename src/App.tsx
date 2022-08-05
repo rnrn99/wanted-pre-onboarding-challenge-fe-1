dea2e8846f4b824ea9332664a425835a1a63008c
rnrn99 <giu0213@naver.com>
2022-08-05 21:58:04 +0900

    [Add] reset.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./reset.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import LandingPage from "./pages/LandingPage";

8832322ced15fc1f31f852ab3237372a125adba8
rnrn99 <giu0213@naver.com>
2022-08-05 21:55:26 +0900

    [Add] react-query setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import "./App.css";
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import LandingPage from "./pages/LandingPage";
 import AuthPage from "./pages/AuthPage";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/auth" element={<AuthPage />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
